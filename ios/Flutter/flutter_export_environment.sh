481e7230e3ba181dafafcb53140197cb9e47ecde
Aditya Chopra <achopra1999@gmail.com>
2020-11-11 00:02:45 +0530

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/aditya/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/media/aditya/154a17e7-13f9-4d71-88c6-2b4157c428e4/AndroidStudioProjects/FootballSchedule-master"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/aditya/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
